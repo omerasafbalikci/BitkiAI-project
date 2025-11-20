ad0ee678c0f623a4e3be665cce68fe60d88d61fe
omerasafbalikci <balikciomerasaf@gmail.com>
2025-11-20 14:55:31 +0300

    first commit

---
+++ src/main/java/ai/bitki/orchestration/BitkiAiApplication.java
@@ -0,0 +1,13 @@
+package ai.bitki.orchestration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitkiAiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BitkiAiApplication.class, args);
+  }
+
+}
---
+++ src/test/java/ai/bitki/orchestration/BitkiAiApplicationTests.java
@@ -0,0 +1,13 @@
+package ai.bitki.orchestration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BitkiAiApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
