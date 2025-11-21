8f787600ee5402de9dd18729ae35cfb78e75cded
Ömer Asaf BALIKÇI <asaf.balikci@formica.ai>
2025-11-22 00:34:38 +0300

    Polish

--- src/main/java/ai/bitki/orchestration/plant/manager/DefaultPlantManager.java
+++ src/main/java/ai/bitki/orchestration/plant/manager/DefaultPlantManager.java
@@ -1,7 +1,4 @@
 package ai.bitki.orchestration.plant.manager;
 
-import ai.bitki.orchestration.common.dto.LifecycleManager;
-import ai.bitki.orchestration.plant.dto.PlantComponent;
-
-public class DefaultPlantManager implements LifecycleManager<PlantComponent> {
+public class DefaultPlantManager {
 }
